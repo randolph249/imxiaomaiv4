a5b931890d1189171803cb91409ae23fd22188e7
显庆 <chao.guo@alibaba-inc.com>
2015-10-12 22:13:47 +0800

    搜索功能开发

--- assets/views/search/searchresult.js
+++ assets/views/search/searchresult.js
@@ -6,11 +6,13 @@ angular.module('xiaomaiApp').controller('searchresultCtrl', [
   '$state',
   'xiaomaiService',
   'schoolManager',
-  function($scope, $state, xiaomaiService, schoolManager) {
+  'xiaomaiMessageNotify',
+  function($scope, $state, xiaomaiService, schoolManager,
+    searchresultheightupdate) {
     $scope.$on('$stateChangeSuccess', function(e, toState, toParam) {
       $scope.searchkey = decodeURIComponent(toParam.key);
       $scope.isloading = true;
-      loadSku(toParam.key, toParam.page);
+      loadSku(toParam.key);
     });
 
     $scope.gohomepage = function($event) {
@@ -27,22 +29,25 @@ angular.module('xiaomaiApp').controller('searchresultCtrl', [
     };
 
     //加载搜索结果
-    var loadSku = function(key, page) {
+    var loadSku = function(key) {
+      $scope.isloading = true;
       var collegeId;
       schoolManager.get().then(function(res) {
         collegeId = res.collegeId;
         return xiaomaiService.fetchOne('searchresult', {
           collegeId: collegeId,
           key: key,
-          currentPage: page
+          currentPage: 1
         });
       }).then(function(res) {
         $scope.goods = res.goods;
         $scope.paginationInfo = res.paginationInfo;
+        $scope.haserror = !$scope.goods || !$scope.goods.length;
+      }, function() {
+        $scope.haserror = true;
+      }).finally(function() {
+        $scope.isloading = false;
       });
-      xiaomaiService.fetchOne('searchsuggest', {
-
-      })
     };
 
 
