ad7d106b24244c7a0f969c996f79383c5fbd788d
显庆 <chao.guo@alibaba-inc.com>
2015-11-07 15:39:49 +0800

    合并代码完成

--- assets/service/ajaxService.js
+++ assets/service/ajaxService.js
@@ -215,34 +215,8 @@ angular.module('xiaomaiApp').factory('xiaomaimodelManage', function() {
       "usercenter": {
         url: "/wap/usercenter/index",
         type: "GET"
-      },
-      //模板多商品活动
-      'complexGoods': {
-        url: '/wap/activity/popularGoods',
-        type: 'GET'
-      },
-      //模板单商品活动
-      'singleGoods': {
-        url: '/wap/activity/brandGoods',
-        type: 'GET'
-      },
-      //淘精品导航
-      "taoBestNavMenu": {
-        url: "/wap/menu/navigationMenu",
-        type: "GET"
-      },
-      //淘精品获取某个菜单下商品列表接口
-      "taoBestGoods": {
-        url: "/wap/menu/navigationMenu/goods",
-        type: "GET"
-      },
-      //淘精品获取某个菜单下活动列表接口
-      "taoBestActivity": {
-        url: "/wap/menu/navigationMenu/activity",
-        type: "GET"
       }
     },
-
     getModel = function() {
       var args = Array.prototype.slice.call(arguments, 0),
         name = args[0],
@@ -264,11 +238,15 @@ angular.module('xiaomaiApp').factory('xiaomaimodelManage', function() {
  *如果是当前环境是线下环境 拦截URL转成对应的JS文件
  **/
 angular.module('xiaomaiApp').factory('urlInterceptor', ['env', function(env) {
+  var xiaomai_schoolid = 99999999;
+  var searchParam = window.location.search;
+  var reg = /xiaomai_schoolid+=([^\?&#])/;
+  xiaomai_schoolid = angular.isArray(searchParam.match(reg)) ? searchParam.match(reg)[1] : xiaomai_schoolid;
   var interceptor = function(url) {
     if (env !== 'develop') {
-      return url;
+      return url + '?xiaomai_schoolid=' + xiaomai_schoolid;
     }
-    return '/api' + url + '.json';
+    return '/api' + url + '.json?xiaomai_schoolid=' + xiaomai_schoolid;
 
   };
   return interceptor;
